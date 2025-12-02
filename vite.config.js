383c6f4aae0c450823955f90403b38129731b935
ARounder <2892375641@qq.com>
2025-12-02 17:15:15 +0800

    feat(config): load environment variables based on mode in vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,29 @@
 import path from "path";
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-    base: process.env.BK_SITE_URL || "/",
-    plugins: [react()],
-    server: {
-        port: 3000,
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+    // 根据当前模式加载 .bk.{mode}.env 文件
+    const env = loadEnv(mode, process.cwd(), ".bk");
+
+    return {
+        base: env.BK_SITE_URL || "/",
+        plugins: [react()],
+        server: {
+            port: 3000,
         },
-    },
-    build: {
-        outDir: "build",
-    },
-    esbuild: {
-        loader: "jsx",
-        include: /src\/.*\.jsx?$/,
-        exclude: [],
-    },
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src"),
+            },
+        },
+        build: {
+            outDir: "build",
+        },
+        esbuild: {
+            loader: "jsx",
+            include: /src\/.*\.jsx?$/,
+            exclude: [],
+        },
+    };
 });
